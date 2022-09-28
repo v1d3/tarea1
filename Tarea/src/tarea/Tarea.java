4fbdb0600da07e33c45fd9586ad2762922b7d9b4
Yulissa <ysanhueza2019@udec.cl>
2022-09-28 19:14:00 -0300

    ola

---
+++ Tarea/src/tarea/Tarea.java
@@ -0,0 +1,54 @@
+
+package tarea;
+import java.util.Date;
+
+public class Tarea {
+
+    public static void main(String[] args) {
+        
+    }    
+}
+
+class OrdenCompra{
+   private Date fecha;
+   private String estado;
+   
+   public void calcPrecioSinIVA(){
+       
+   }
+   public void calcIVA(){
+       
+   }
+   public void calcPrecio(){
+   }
+   public void calcPeso(){
+       
+   } 
+}
+
+
+class DetalleOrden{
+    private int cantidad;
+     public void calcPrecio(){
+   }
+    public void calcPrecioSinIVA(){
+       
+   }
+   public void calcIVA(){
+       
+   }
+  
+   public void calcPeso(){
+       
+   } 
+    
+}
+
+class Articulo{
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+}
+
+class Cliente
