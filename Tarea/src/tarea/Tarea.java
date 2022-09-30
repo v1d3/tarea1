155c33e77af96d6ffedfe32ce2f8b88c78d4cf6d
v1d3 <ccristobalc132@gmail.com>
2022-09-30 17:41:52 -0300

    Agregué algunas clases además de estar probando el uso del merge

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -1,54 +1,86 @@
 
 package tarea;
+
 import java.util.Date;
 
+//Main
 public class Tarea {
 
     public static void main(String[] args) {
-        
-    }    
+
+    }
 }
+//Clases
 
-class OrdenCompra{
-   private Date fecha;
-   private String estado;
-   
-   public void calcPrecioSinIVA(){
-       
-   }
-   public void calcIVA(){
-       
-   }
-   public void calcPrecio(){
-   }
-   public void calcPeso(){
-       
-   } 
-}
-
-
-class DetalleOrden{
-    private int cantidad;
-     public void calcPrecio(){
-   }
-    public void calcPrecioSinIVA(){
-       
-   }
-   public void calcIVA(){
-       
-   }
-  
-   public void calcPeso(){
-       
-   } 
+class OrdenCompra {
     
+    private Date fecha;
+    private String estado;
+
+    public void calcPrecioSinIVA() {
+
+    }
+
+    public void calcIVA() {
+
+    }
+
+    public void calcPrecio() {
+    }
+
+    public void calcPeso() {
+
+    }
 }
 
-class Articulo{
+class DetalleOrden {
+
+    private int cantidad;
+
+    public void calcPrecio() {
+    }
+
+    public void calcPrecioSinIVA() {
+
+    }
+
+    public void calcIVA() {
+
+    }
+
+    public void calcPeso() {
+
+    }
+
+}
+
+class Articulo {
+
     private float peso;
     private String nombre;
     private String descripcion;
     private float precio;
 }
 
-class Cliente
+class Cliente {
+
+}
+
+class Direccion{
+    
+}
+
+class DocTributario{
+    
+}
+
+class Boleta{
+    
+}
+class Factura{
+    
+}
+
+class Pago{
+    
+}
